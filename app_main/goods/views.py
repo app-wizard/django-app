69114569843c41b7cf786973017026e8d58cf89c
Volodymyr Popov <texnolife@gmail.com>
2024-02-05 12:06:50 +0000

    add product filter

--- app_main/goods/views.py
+++ app_main/goods/views.py
@@ -4,14 +4,24 @@ from goods.models import Products
 
 
 # Create your views here.
-def catalog(request, category_slug, page=1):
-    if category_slug == 'all':
+def catalog(request, category_slug):
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
+
+    if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        goods.order_by(order_by)
+
     paginator = Paginator(goods, 3)
-    curent_page = paginator.page(page)
+    curent_page = paginator.page(int(page))
 
     context = {
         "title": "Каталог товаров",
