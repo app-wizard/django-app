145019950ca0ffc04683e672b8e850fabccf323c
Volodymyr Popov <texnolife@gmail.com>
2024-02-08 12:57:32 +0000

    add login form + controller

--- app_main/main/urls.py
+++ app_main/main/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path
 from main import views
 
-app_name = 'main'
+app_name = "main"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
 ]
---
+++ app_main/users/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+
+    # username = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "autofocus": True,
+    #             "class": "form-control",
+    #             "placeholder": "Username",
+    #         }
+    #     )
+    # )
+    # password = forms.CharField(
+    #     label=("Password"),
+    #     strip=False,
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "autocomplete": "current-password",
+    #             "class": "form-control",
+    #             "placeholder": "Password",
+    #         }
+    #     ),
+    # )
+
+    class Meta:
+        model = User
+        fields = ("username", "password")
