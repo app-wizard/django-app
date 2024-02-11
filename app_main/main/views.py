bb706738df1da4f81a7d2a13321160a27ad426d3
Volodymyr Popov <texnolife@gmail.com>
2024-02-11 15:21:50 +0000

    lang fixes

--- app_main/main/views.py
+++ app_main/main/views.py
@@ -7,7 +7,7 @@ def index(request,) -> HttpResponse:
 
     context = {
         "title": "Home - main",
-        "content": "Furniture and accessories",
+        "content": "Step into a world where every bite tells a story of centuries-old traditions",
     }
     return render(request, "main/index.html", context)
 
@@ -16,6 +16,6 @@ def about(request):
     context = {
         "title": "about",
         "content": "About page",
-        "text_on_page": "Раздел «О нас» дает посетителям сайта представление о компании или личности. Цифры, кейсы, ссылки на крупных заказчиков — подтвержденная информация повышает ваш авторитет и формирует доверие со стороны аудитории. Здесь вы показываете свою значимость и объясняете, почему выгодно быть вашим клиентом.Этот раздел подходит для краткого рассказа о команде и истории развития компании. Покажите лица, которые стоят за продуктом — открытость расположит к вам людей. Раскройте свою миссию. Например, вы не просто торгуете техникой, а имеете цель — облегчить быт и улучшить качество жизни. Если есть награды и сертификаты, разместите их здесь.Есть вероятность, что сайт могут найти по ключевикам на странице, описывающей вашу деятельность. Учитывайте это при составлении текста. Возможно, вам пригодится этот материал: «SEO-текст: как писать оптимизированные и полезные статьи». Постарайтесь также, чтобы его было интересно читать. Чем больше времени пользователь проведет у вас, тем лучше будет позиция сайта в выдаче.",
+        "text_on_page": "Welcome to Lotus Pavilion, where the spirit of traditional Chinese cuisine is reimagined with modern elegance. Nestled in the vibrant cityscape of Azure Bay, Lotus Pavilion stands as a beacon of culinary innovation and heritage. Chef Ming Zhao, our visionary founder, embarked on a quest to blend the rich flavors of his Chinese roots with contemporary culinary techniques. His journey, fueled by the diverse tastes of China's regions, led to the creation of a unique dining experience that honors the past while embracing the future. At Lotus Pavilion, our menu is a celebration of contrast and harmony. From the delicate balance of Cantonese dim sum to the fiery zest of Sichuan dishes, our chefs expertly craft each meal using fresh, locally-sourced ingredients, ensuring a symphony of flavors in every bite. The restaurant's ambiance reflects a fusion of tradition and modernity, providing a warm, welcoming space for guests to explore the wonders of Chinese cuisine. Lotus Pavilion is not just a place to eat; it's a destination where culinary dreams come to life, offering a memorable journey through the heart of Chinese gastronomy. Join us at Lotus Pavilion, where every meal is a masterpiece, and every visit is an adventure in flavor.",
     }
     return render(request, "main/about.html", context)
